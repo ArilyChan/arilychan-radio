223b5c14388c02fd698a16493d2875b963d3cfa6
Starlight Exsper <exsper@163.com>
2020-10-23 22:01:47 +0800

    add delete & broadcast

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ module.exports.webPath = '/radio';
 module.exports.init = (option = {}) => {
     const playlist = new Map()
     const emitter = new EventEmitter()
-    const removeAfterDays = (( option.expire || 7 ) + 1)
+    const removeAfterDays = ((option.expire || 7) + 1)
 
     return {
         emitter,
+        /**
+         * 点歌
+         * @param {String} msg “点歌”后面的参数
+         * @returns {import("./lib/api/sayobot").BeatmapInfo} BeatmapInfo
+         */
         async search(msg) {
             const arg = new Arg(msg);
             const beatmapInfo = await arg.getBeatmapInfo();
@@ -20,8 +25,30 @@ module.exports.init = (option = {}) => {
             setTimeout(() => emitter.emit('search-result', beatmapInfo), 0) // give bot time to setup qq and name
             return beatmapInfo;
         },
+        /**
+         * 从playlist中删除指定歌曲
+         * @param {Number} uuid 
+         * @param {Number} qqId -1=可以删除任何人的歌曲，管理员限定
+         */
+        async delete(uuid, qqId = -1) {
+            let aim = Array.from(playlist).filter(([sid, song]) => {
+                if (qqId === -1) return (song.uuid === uuid)
+                else return (song.uuid === uuid && song.uploader.id === qqId)
+            });
+            if (aim.length <= 0) throw "找不到该曲目";
+            // aim.length should be 1
+            playlist.delete(aim.pop().sid);
+        },
+        /**
+         * 广播
+         * @param {Number|String} qqId qqId或其他东西
+         * @param {String} msg 
+         */
+        async broadcast(qqId, msg) {
+            setTimeout(() => emitter.emit('broadcast-message', {name: qqId}, msg), 0)
+        },
         playlist,
-        filteredPlaylistArray(){
+        filteredPlaylistArray() {
             const now = new Date()
             return Array.from(playlist)
                 .filter(([sid, song]) => {
@@ -32,12 +59,14 @@ module.exports.init = (option = {}) => {
                     if (shouldRemove) playlist.delete(sid)
                     return !shouldRemove
                 })
-            .map(([sid, result]) => result)
-            .reverse()
+                .map(([sid, result]) => result)
+                .reverse()
         }
     }
 }
 module.exports.webView = server
+
+let uuid = 0;
 module.exports.apply = (ctx, options, storage) => {
     ctx.middleware(async (meta, next) => {
         try {
@@ -47,26 +76,54 @@ module.exports.apply = (ctx, options, storage) => {
             if (command[0].substring(0, 1) !== '!' && command[0].substring(0, 1) !== '！') return next();
             if (command[0].length < 2) return next();
             const act = command[0].substring(1);
-            if (act === '点歌') {
-                try {
-                    // TODO
-                    let beatmapInfo = await storage.search(command.slice(1).join(' '));
-                    beatmapInfo.uploader = {
-                        id: userId,
-                        nickname: meta.sender.nickname
-                    };
-                    let reply = `[CQ:at,qq=${userId}]\n`;
-                    reply += "搜索到曲目：" + beatmapInfo.artistU + " - " + beatmapInfo.titleU + "\n";
-                    if (!beatmapInfo.audioFileName) reply += "小夜没给音频，只有试听\n";
-                    reply += "点歌成功！歌曲将会在歌单保存 " + options.expire + " 天";
-                    reply += "\n电台地址：" + options.web.host + options.web.path;
-                    return meta.$send(reply);
-                }
-                catch (ex) {
-                    return meta.$send(`[CQ:at,qq=${userId}]\n` + ex);
-                }
+            switch (act) {
+                case '点歌':
+                case 'audio.queue':
+                    try {
+                        let beatmapInfo = await storage.search(command.slice(1).join(' '));
+                        beatmapInfo.uploader = {
+                            id: userId,
+                            nickname: meta.sender.nickname
+                        };
+                        beatmapInfo.uuid = ++uuid;
+                        let reply = `[CQ:at,qq=${userId}]\n`;
+                        reply += "搜索到曲目：" + beatmapInfo.artistU + " - " + beatmapInfo.titleU + "\n";
+                        if (!beatmapInfo.audioFileName) reply += "小夜没给音频，只有试听\n";
+                        reply += "点歌成功！UUID：" + beatmapInfo.uuid + "，歌曲将会保存 " + options.expire + " 天";
+                        reply += "\n电台地址：" + options.web.host + options.web.path;
+                        return meta.$send(reply);
+                    }
+                    catch (ex) {
+                        return meta.$send(`[CQ:at,qq=${userId}]\n` + ex);
+                    }
+                case '广播':
+                case 'audio.broadcast':
+                    try {
+                        if (!options.isAdmin(meta)) return meta.$send(`[CQ:at,qq=${userId}]\n只有管理员才能发送广播消息`);
+                        const msg = command.slice(1).join(' ');
+                        await storage.broadcast(userId, msg);
+                        return meta.$send(`[CQ:at,qq=${userId}]\n已发送广播`);
+                    }
+                    catch (ex) {
+                        return meta.$send(`[CQ:at,qq=${userId}]\n` + ex);
+                    }
+                case '删歌':
+                case 'audio.delete':
+                    try {
+                        const arg = command.slice(1).join(' ');
+                        if (!arg) return meta.$send(`[CQ:at,qq=${userId}]\n请指定UUID`);
+                        const uuid = parseInt(arg);
+                        if (!uuid) return meta.$send(`[CQ:at,qq=${userId}]\nUUID应该是个正整数`);
+                        if (options.isAdmin(meta)) await storage.delete(uuid);
+                        else await storage.delete(uuid, userId);
+                        return meta.$send(`[CQ:at,qq=${userId}]\n删除成功！`);
+                    }
+                    catch (ex) {
+                        return meta.$send(`[CQ:at,qq=${userId}]\n` + ex);
+                    }
+                default: return next();
+
             }
-            return next();
         } catch (ex) {
             console.log(ex);
             return next();
